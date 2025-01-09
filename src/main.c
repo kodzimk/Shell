2d8cded882c29fc3fb14225638e7809bc1714f25
kodzimk <proggamedev@gmail.com>
2025-01-09 21:18:44 +0500

    omg shell works but only some commands

--- src/main.c
+++ src/main.c
@@ -1,53 +1,82 @@
 #include <stdio.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include<stdlib.h>
 #include<string.h>
+#include<assert.h>
 
+#pragma warning(disable : 4996)
+char** str_split(char* a_str, const char a_delim)
+{
+    char** result = 0;
+    size_t count = 0;
+    char* tmp = a_str;
+    char* last_comma = 0;
+    char delim[2];
+    delim[0] = a_delim;
+    delim[1] = 0;
+
+    /* Count how many elements will be extracted. */
+    while (*tmp)
+    {
+        if (a_delim == *tmp)
+        {
+            count++;
+            last_comma = tmp;
+        }
+        tmp++;
+    }
+
+    /* Add space for trailing token. */
+    count += last_comma < (a_str + strlen(a_str) - 1);
+
+    /* Add space for terminating null string so caller
+       knows where the list of returned strings ends. */
+    count++;
+
+    result = malloc(sizeof(char*) * count);
+
+    if (result)
+    {
+        size_t idx = 0;
+        char* token = strtok(a_str, delim);
+
+        while (token)
+        {
+            assert(idx < count);
+            *(result + idx++) = strdup(token);
+            token = strtok(0, delim);
+        }
+        assert(idx == count - 1);
+        *(result + idx) = 0;
+    }
+
+    return result;
+}
 
 int main()
 {
-    while (1)
-    {
-        char arr[100];
-
+ 
+        char buf[100];
         printf("$ ");
-        fgets(arr,100,stdin);
-        int split = 0;
-        
-        char* command = strtok(arr," "); 
-        fwrite(command, sizeof(command[0]), sizeof(command), stdout);
-        char* argument_list[3];
-        argument_list[0] = command;
+        scanf("%s", buf);
+        char** argument_list = str_split(buf,' ');
 
-        int index = 0;
-        for (int i = 0; i < 100; i++)
+        pid_t process = fork();
+        if (process == 0)
         {
-            if (arr[i] == ' ')
-            {
-                split = 1;
-                continue;
-            }
-            else if(arr[i] == ' ' && split == 1)
-                break;
-
-            if (split == 1 && arr[i] != ' ')
-            {
-                argument_list[2][index] = arr[i];
-                index++;
-            }
-        }
-        argument_list[2] = NULL;
-
-        pid_t p = fork();
-        if (p == 0) {
-            execvp(command, argument_list);
+            perror("Child process");
+            execvp(argument_list[0], argument_list);
+            exit(0);
         }
         else
         {
-            printf("Hello world!, process_id(pid) = %d \n", getpid());
-         
+            printf("PID of process: %d\n", process);
+            int i = 1;
+            wait(&i);
         }
-    }
+    
+
     return 0;
 }
